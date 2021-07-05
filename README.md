22111094684c5b46f83124bd567fdadc72d0b3ba
yuanjingsong <532473857@qq.com>
2021-07-05 22:28:23 +0800

    add new info

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Related Paper:
 
 * Wen Xia, Xiangyu Zou, Yukun Zhou, Hong Jiang, Chuanyi Liu, Dan Feng, Yu Hua, Yuchong Hu, Yucheng Zhang, "The Design of Fast Content-Defined Chunking for Data Deduplication based Storage Systems", IEEE Transactions on Parallel and Distributed Systems (TPDS), 2020. [LINK](https://ieeexplore.ieee.org/document/9055082)
 
+欢迎商用，但请邮件 xiawen@hit.edu.cn  告知，并报告您遇到的问题；
+
 
 ### Dependency
 * openssl
--- fastcdc.h
+++ fastcdc.h
@@ -1,3 +1,10 @@
+/**
+ * This is an implementation of fastCDC
+ * The origin paper is Wen Xia, Yukun Zhou, Hong Jiang, Dan Feng, Yu Hua, Yuchong Hu, Yucheng Zhang, Qing Liu, "FastCDC: a Fast and Efficient Content-Defined Chunking Approach for Data Deduplication", in Proceedings of USENIX Annual Technical Conference (USENIX ATC'16), Denver, CO, USA, June 22–24, 2016, pages: 101-114
+ * and Wen Xia, Xiangyu Zou, Yukun Zhou, Hong Jiang, Chuanyi Liu, Dan Feng, Yu Hua, Yuchong Hu, Yucheng Zhang, "The Design of Fast Content-Defined Chunking for Data Deduplication based Storage Systems", IEEE Transactions on Parallel and Distributed Systems (TPDS), 2020
+ *
+ */ 
+
 #include <openssl/md5.h>
 #include <openssl/sha.h>
 #include <stdio.h>
